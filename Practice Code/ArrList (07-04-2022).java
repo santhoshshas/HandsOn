8e45989094e4ed18f93b80338886d2ee2ac95a08
santhoshshas <95738382+santhoshshas@users.noreply.github.com>
2022-04-08 09:04:29 +0530

    Add files via upload

---
+++ Practice Code/ArrList (07-04-2022).java
@@ -0,0 +1,22 @@
+package b;
+
+import java.util.ArrayList;
+import java.util.ListIterator;
+
+class ArrList {
+  public static void main(String[] args) {
+    // Creating an ArrayList
+    ArrayList<Integer> numbers = new ArrayList<>();
+    numbers.add(1);
+    numbers.add(3);
+    numbers.add(2);
+    System.out.println("ArrayList: " + numbers);
+
+    // Creating an instance of ListIterator
+    ListIterator<Integer> iterate = numbers.listIterator();
+    System.out.println("Iterating over ArrayList:");
+    while(iterate.hasNext()) {
+      System.out.print(iterate.next() + ", ");
+    }
+  }
+}
